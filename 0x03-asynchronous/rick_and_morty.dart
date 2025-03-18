2e418bd746f13316ee0e63937ce360cc0ea6490a
Eidal559 <eidaljuarbe@gmail.com>
2025-03-17 21:23:10 -0400

    fix

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,23 +1,16 @@
+import "package:http/http.dart" as http;
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 
-Future<void> printRmCharacters() async {
-  const String apiUrl = 'https://rickandmortyapi.com/api/character';
-
+printRmCharacters() async {
   try {
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final List characters = data['results'];
-
-      for (var character in characters) {
-        print(character['name']);
-      }
-    } else {
-      print('Error: Failed to load characters');
+    final res = await http.get(
+      Uri.parse('https://rickandmortyapi.com/api/character'),
+    );
+    var json = jsonDecode(res.body);
+    for (var idx = 0; idx < json['results'].length; idx++) {
+      print("${json['results'][idx]['name']}");
     }
-  } catch (error) {
-    print('error caught: $error');
+  } catch (err) {
+    print('error caught: $err');
   }
 }
